b98da977f95d35e7a976d810b2575542fb73a069
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-08 08:00:00 -0400

    refactor tests.py to include setUp method te remove redundancy and added 1 more test case

--- ch4-bookstore/pages/tests.py
+++ ch4-bookstore/pages/tests.py
@@ -1,24 +1,26 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        resp = self.client.get("/")
-        self.assertEqual(resp.status_code, 200)
+    def setUp(self):
+        url = reverse("home")
+        self.resp = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        resp = self.client.get(reverse("home"))
-        self.assertEqual(resp.status_code, 200)
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.resp.status_code, 200)
 
     def test_homepage_template(self):
-        resp = self.client.get("/")
-        self.assertTemplateUsed(resp, "home.html")
+        self.assertTemplateUsed(self.resp, "home.html")
 
     def test_homepage_contains_correct_html(self):
-        resp = self.client.get("/")
-        self.assertContains(resp, "home page")
+        self.assertContains(self.resp, "home page")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        resp = self.client.get("/")
-        self.assertNotContains(resp, "Hi there! I should not be on the page.")
+        self.assertNotContains(self.resp, "Hi there! I should not be on the page.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
