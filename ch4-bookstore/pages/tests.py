aad9b5c7e939e45e5d416d139540a0db14f9f87d
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-11 08:00:00 -0400

    ch7 feature: created about page with its template, views, route in urls and test cases

--- ch4-bookstore/pages/tests.py
+++ ch4-bookstore/pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -24,3 +24,25 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.resp = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.resp, "about.html")
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.resp, "About Page")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.resp, "Hi there! I should not be on the page.")
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
