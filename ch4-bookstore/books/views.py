5dd5a74999cd1796362f0b28ea8791e2ac3c7803
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-13 16:48:34 -0400

    ch11: created book list page with its corresponsing url route, view, and template

--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    # more friendly name. default is 'object_list' in template and contains all objects in the view
+    context_object_name = "book_list"
+    template_name = "books/book_list.html"
