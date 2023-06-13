9e38afd26993678b7bacb4cdae2f3af1312e5888
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-13 17:05:11 -0400

    ch11: created book detail page with its corresponsing url route, view, and template

--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Book
 
@@ -9,3 +9,9 @@ class BookListView(ListView):
     # more friendly name. default is 'object_list' in template and contains all objects in the view
     context_object_name = "book_list"
     template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = "book"
+    template_name = "books/book_detail.html"
